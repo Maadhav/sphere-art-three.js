b9590022bf47b0c3004cd84a6939a622a269b3ee
Maadhav Sharma <maadhav2001@gmail.com>
2021-09-02 17:54:38 +0530

    docs updoated

--- editor/js/Menubar.Help.js
+++ editor/js/Menubar.Help.js
@@ -61,7 +61,7 @@ function MenubarHelp( editor ) {
 	option.setTextContent( strings.getKey( 'menubar/help/manual' ) );
 	option.onClick( function () {
 
-		window.open( 'https://sphereart-editor.netlify.app/editor/docs/docs.md', '_blank' );
+		window.open( 'https://github.com/Maadhav/sphere-art-three.js/blob/master/editor/docs/docs.md', '_blank' );
 
 	} );
 	options.add( option );
