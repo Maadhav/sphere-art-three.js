3d62296e11f4bfdc5841120bfd84cc37894767ef
kunal528 <jainkunal976@gmail.com>
2021-12-07 15:11:57 +0530

    import ui sidebar created

--- editor/js/Menu.Sidebar.js
+++ editor/js/Menu.Sidebar.js
@@ -1,21 +1,27 @@
 import { UIDiv, UIText } from "./libs/ui.js";
 import MenuSidebarAdd from "./Menu.SideBar.Add.js";
+import MenuSidebarImport from "./Menu.Sidebar.Import.js";
 
 function MenuSidebar(editor) {
     var container = new UIDiv();
     container.setId("menu-sidebar");
 
     document.addEventListener("click", function (e) {
+        // console.log(e.target)
         var sidebar = document.getElementById("menu-sidebar");
         var element = document.getElementsByClassName('menu-options active')[0];
-        if (element.dataset.icon == 'add') {
-            var menubar_add = MenuSidebarAdd(editor)
-
-            sidebar.replaceChildren(menubar_add.dom);
-        }
-        else {
-            var div = new UIDiv();
-            sidebar.replaceChildren(div.dom);
+        switch (element.dataset.icon) {
+            case "add":
+                var menubar_add = MenuSidebarAdd(editor)
+                sidebar.replaceChildren(menubar_add.dom);
+                break;
+            case "import":
+                var menubar_import = MenuSidebarImport(editor)
+                sidebar.replaceChildren(menubar_import.dom);
+                break;
+            default:
+                var div = new UIDiv();
+                sidebar.replaceChildren(div.dom);
         }
     })
     return container;
