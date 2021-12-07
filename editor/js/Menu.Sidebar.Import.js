ebde407c1a0418d26292d7fdacdd1281b7e873a5
kunal528 <jainkunal976@gmail.com>
2021-12-07 22:28:07 +0530

    changing text

--- editor/js/Menu.Sidebar.Import.js
+++ editor/js/Menu.Sidebar.Import.js
@@ -12,7 +12,8 @@ export default function MenuSidebarImport(editor) {
 	importContainer.addClass("container");
 	var importInput = document.createElement("input");
 	importInput.type = "file";
-	importInput.style.display = "none";
+	importInput.id = "import-input";
+	// importInput.style.display = "none";
 
 	var image = document.createElement("img");
 	image.src = "./images/import.svg";
@@ -20,10 +21,18 @@ export default function MenuSidebarImport(editor) {
 
 	var text = new UIDiv();
 	text.setTextContent("Upload");
+	text.setId("import-text");
 	text.addClass("text");
 	importContainer.onClick(function () {
 		importInput.click();
 	});
+
+	importInput.addEventListener("change", function (event) {
+		var element = document.getElementById("import-input");
+		element.replaceWith(event.target)
+		var element = document.getElementById('import-text')
+		element.innerHTML = "Importing..."
+	})
 
 	importContainer.dom.append(importInput, image, text.dom);
 
