d902607d39f891b4fd48b79d86924a712375882c
Maadhav Sharma <maadhav2001@gmail.com>
2021-12-07 16:42:38 +0530

    removed pointer-events: none;

--- editor/js/Menu.Sidebar.Import.js
+++ editor/js/Menu.Sidebar.Import.js
@@ -1,40 +1,35 @@
 import { UIDiv, UIInput } from "./libs/ui.js";
 
 export default function MenuSidebarImport(editor) {
-    var container = new UIDiv();
-    container.setId("menu-sidebar-import");
+	var container = new UIDiv();
+	container.setId("menu-sidebar-import");
 
+	var importHeader = new UIDiv();
+	importHeader.addClass("header");
+	importHeader.setTextContent("Import");
 
-    var importHeader = new UIDiv();
-    importHeader.addClass("header");
-    importHeader.setTextContent("Import");
+	var importContainer = new UIDiv();
+	importContainer.addClass("container");
+	var importInput = document.createElement("input");
+	importInput.type = "file";
+	importInput.style.display = "none";
 
+	var image = document.createElement("img");
+	image.src = "./images/import.svg";
+	image.classList.add("icon");
 
-    var importContainer = new UIDiv();
-    importContainer.addClass("container");
-    var importInput = document.createElement("input");
-    importInput.type = "file";
-    importInput.style.display = "none";
+	var text = new UIDiv();
+	text.setTextContent("Upload");
+	text.addClass("text");
+	importContainer.onClick(function () {
+		importInput.click();
+	});
 
+	importContainer.dom.append(importInput, image, text.dom);
 
-    var image = document.createElement("img");
-    image.src = "./images/import.svg";
-    image.classList.add("icon");
-
-    var text = new UIDiv()
-    text.setTextContent("Upload");
-    text.addClass("text");
-    image.addEventListener('click', function () {
-        console.log("click");
-        importInput.click();
-    })
-
-    importContainer.dom.append(importInput, image, text.dom);
-
-
-    var importButton = new UIDiv();
-    importButton.addClass("button");
-    importButton.setTextContent("Import");
-    container.add(importHeader, importContainer, importButton);
-    return container;
+	var importButton = new UIDiv();
+	importButton.addClass("button");
+	importButton.setTextContent("Import");
+	container.add(importHeader, importContainer, importButton);
+	return container;
 }
